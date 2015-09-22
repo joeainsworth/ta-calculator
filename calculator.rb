002a4e158e596977b803c1bba40d73a7f5bd16a6
Joe Ainsworth <joe.ainsworth@me.com>
2015-09-22 22:12:23 +0100

    Added validation methods

--- calculator.rb
+++ calculator.rb
@@ -2,38 +2,54 @@ def say(msg)
   puts "\n=> #{msg}"
 end
 
-def input
+def validate_number(input)
+  return input if input.to_s =~ /[0-9]/
+end
+
+def retrieve_number(number, numbers)
+  numbers << number if validate_number(number)
+end
+
+def get_numbers
   numbers = []
-  say "Please input your first number: "
-  numbers << gets.chomp
-  say "Please input your second number: "
-  numbers << gets.chomp
+  loop do
+    say "Please enter your first number: "
+    break if retrieve_number(gets.chomp, numbers)
+  end
+  loop do
+    say "Please enter your second number: "
+    break if retrieve_number(gets.chomp, numbers)
+  end
+  numbers
 end
 
 def operation
   say "Available operations"
   puts "[1] + [2] - [3] * [4] /"
-  say "Please the number of the operation you want to perform: "
-  gets.chomp
+  loop do
+    say "Please enter the number of the operation you want to perform: "
+    operator = gets.chomp
+    return operator if validate_number(operator)
+  end
 end
 
 def calculate(numbers, operator)
   case operator.to_i
   when 1
-    numbers[0].to_i + numbers[1].to_i
+    numbers.first.to_i + numbers.last.to_i
   when 2
-    numbers[0].to_i - numbers[1].to_i
+    numbers.first.to_i - numbers.last.to_i
   when 3
-    numbers[0].to_i * numbers[1].to_i
+    numbers.first.to_i * numbers.last.to_i
   when 4
-    numbers[0].to_f / numbers[1].to_f
+    numbers.first.to_f / numbers.last.to_f
   else
     "Please enter a valid operation!"
   end
 end
 
 loop do
-  numbers     = input
+  numbers     = get_numbers
   operator    = operation
 
   say "Your calculation:"
